384361f02b869fa529a2a1058d8baeb08bfc17b1
hendrixjoseph <hendixjoseph@aol.com>
2024-06-26 15:12:49 -0400

    update test name

--- src/test/java/com/joehxblog/action/CheckoutTest.java
+++ src/test/java/com/joehxblog/action/CheckoutTest.java
@@ -15,14 +15,13 @@ class CheckoutTest {
 
     @ParameterizedTest
     @CsvSource({
-            "JAKR, 2015, 9, 3, 5, 101",
             "LADW, 2020, 7, 2, 3, 10",
             "CHNS, 2015, 7, 2, 5, 0",
             "JAKD, 2015, 9, 3, 6, 0",
             "JAKR, 2015, 7, 2, 9, 0",
             "JAKR, 2020, 7, 2, 4, 50"
     })
-    void test(String toolCode, int year, int month, int day, int rentalDays, int discount) {
+    void testCheckout(String toolCode, int year, int month, int day, int rentalDays, int discount) {
         var rentalAgreement = checkout.checkout(
                 toolCode,
                 rentalDays,
