7f3cb0713c8e22636d30ba86cb542e43bbad1e5c
hendrixjoseph <hendixjoseph@aol.com>
2024-06-25 21:43:26 -0400

    create some calc charge days methods

--- src/main/java/com/joehxblog/action/Checkout.java
+++ src/main/java/com/joehxblog/action/Checkout.java
@@ -1,15 +1,23 @@
 package com.joehxblog.action;
 
+import com.joehxblog.entity.ToolType;
 import com.joehxblog.entity.Tools;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.stream.IntStream;
+
+import static java.time.DayOfWeek.*;
 
 public class Checkout {
     private Tools tools = new Tools();
 
     public RentalAgreement checkout(String toolCode, int rentalDayCount, int discountPercent, LocalDate checkoutDate) {
+        var tool = tools.getTool(toolCode);
+
         return new RentalAgreement(
-                tools.getTool(toolCode),
+                tool,
                 rentalDayCount,
                 discountPercent,
                 checkoutDate,
@@ -17,7 +25,37 @@ public class Checkout {
         );
     }
 
-    private LocalDate calculateDueDate(LocalDate checkoutDate, int rentalDayCount) {
+    public int calculateChargeDays(LocalDate checkoutDate, int rentalDayCount, ToolType toolType) {
+        return (int) IntStream.range(0, rentalDayCount)
+                .mapToObj(checkoutDate::plusDays)
+                .filter(date -> !toolType.weekendCharge() || isWeekend(date))
+                .filter(date -> !toolType.holidayCharge() || isHoliday(date))
+                .count();
+    }
+
+    public boolean isWeekend(LocalDate date) {
+        var weekday = date.getDayOfWeek();
+
+        return weekday == SATURDAY || weekday == SUNDAY;
+    }
+
+    public boolean isHoliday(LocalDate date) {
+        return isIndependenceDay(date) || isLaborDay(date);
+    }
+
+    public boolean isLaborDay(LocalDate date) {
+        return date.getDayOfMonth() < 8 && date.getDayOfWeek() == MONDAY;
+    }
+
+    public boolean isIndependenceDay(LocalDate date) {
+        return date.getMonth() == Month.JULY && (
+                date.getDayOfWeek() == FRIDAY && date.getDayOfMonth() == 3
+             || date.getDayOfWeek() == MONDAY && date.getDayOfMonth() == 5
+             || !isWeekend(date) && date.getDayOfMonth() == 4
+        );
+    }
+
+    public LocalDate calculateDueDate(LocalDate checkoutDate, int rentalDayCount) {
         return checkoutDate.plusDays(rentalDayCount);
     }
 }
