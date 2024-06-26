2c7fde12d54fba91f3fa6841d78c8db0e4d64fa9
hendrixjoseph <hendixjoseph@aol.com>
2024-06-25 21:07:41 -0400

    create tests

--- src/test/java/com/joehxblog/action/CheckoutTest.java
+++ src/test/java/com/joehxblog/action/CheckoutTest.java
@@ -1,22 +1,38 @@
 package com.joehxblog.action;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckoutTest {
 
+    static Stream<Arguments> testValues() {
+        return Stream.of(
+                Arguments.of("JAKR", 2015, 9, 3, 5, 101),
+                Arguments.of("LADW", 2020, 7, 2, 3, 10),
+                Arguments.of("CHNS", 2015, 7, 2, 5, 0),
+                Arguments.of("JAKD", 2015, 9, 3, 6, 0),
+                Arguments.of("JAKR", 2015, 7, 2, 9, 0),
+                Arguments.of("JAKR", 2020, 7, 2, 4, 50)
+        );
+    }
+
     Checkout checkout = new Checkout();
 
-    @Test
-    void test() {
+    @ParameterizedTest
+    @MethodSource("testValues")
+    void test(String toolCode, int year, int month, int day, int rentalDays, int discount) {
         var rentalAgreement = checkout.checkout(
-                "JAKR",
-                5,
-                101,
-                LocalDate.of(2015, 9, 3)
+                toolCode,
+                rentalDays,
+                discount,
+                LocalDate.of(year, month, day)
         );
 
         System.out.println(rentalAgreement);
